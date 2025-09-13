cc4c61aee14de7ca05369b8709085e0bb02f660e
Ali Ramazon <mr.safarali@gmail.com>
2025-09-12 23:48:59 -0600

    Create Student  platform page

---
+++ src/pages/student/StudentPlatform.tsx
@@ -0,0 +1,57 @@
+import { CalendarClock, Users } from "lucide-react";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import {
+    SideBar,
+    SideBarLinks,
+    type SideBarLinksGroup,
+} from "../../design-system";
+import { AppLayout, AppPage } from "../components/Layout";
+import { SideBarUser } from "../components/SidebarUser";
+
+const links: SideBarLinksGroup[] = [
+    {
+        title: "Manage",
+        links: [
+            {
+                linkText: "Coaches",
+                linkTo: "coaches",
+                icon: Users,
+            },
+            {
+                linkText: "Bookings",
+                linkTo: "bookings",
+                icon: CalendarClock,
+            },
+        ],
+    },
+];
+
+export const StudentPlatform: React.FC = () => {
+    const user = {
+        firstName: "Ali",
+        lastName: "Ramazon",
+        email: "user@example.com",
+    };
+
+    const logOut = () => {
+        console.log("Log out");
+    };
+    return (
+        <AppLayout>
+            <SideBar>
+                <SideBarUser
+                    details={{
+                        firstName: user?.firstName || "",
+                        lastName: user?.lastName || "",
+                        email: user?.email || "",
+                    }}
+                />
+                <SideBarLinks links={links} logOut={logOut} />
+            </SideBar>
+            <AppPage>
+                <Outlet />
+            </AppPage>
+        </AppLayout>
+    );
+};
