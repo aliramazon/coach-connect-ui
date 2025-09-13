71832c622d0f259a7797e7fbe03e447ec1b9f804
Ali Ramazon <mr.safarali@gmail.com>
2025-09-12 23:48:29 -0600

    Create Coach platform page

--- src/pages/coach/CoachPlatform.tsx
+++ src/pages/coach/CoachPlatform.tsx
@@ -1,5 +1,57 @@
+import { CalendarClock, CalendarDays } from "lucide-react";
 import React from "react";
+import { Outlet } from "react-router-dom";
+import {
+    SideBar,
+    SideBarLinks,
+    type SideBarLinksGroup,
+} from "../../design-system";
+import { AppLayout, AppPage } from "../components/Layout";
+import { SideBarUser } from "../components/SidebarUser";
+
+const links: SideBarLinksGroup[] = [
+    {
+        title: "Manage",
+        links: [
+            {
+                linkText: "Availability",
+                linkTo: "availability",
+                icon: CalendarDays,
+            },
+            {
+                linkText: "Bookings",
+                linkTo: "bookings",
+                icon: CalendarClock,
+            },
+        ],
+    },
+];
 
 export const CoachPlatform: React.FC = () => {
-    return <h1>Calendar View</h1>;
+    const user = {
+        firstName: "Ali",
+        lastName: "Ramazon",
+        email: "user@example.com",
+    };
+
+    const logOut = () => {
+        console.log("Log out");
+    };
+    return (
+        <AppLayout>
+            <SideBar>
+                <SideBarUser
+                    details={{
+                        firstName: user?.firstName || "",
+                        lastName: user?.lastName || "",
+                        email: user?.email || "",
+                    }}
+                />
+                <SideBarLinks links={links} logOut={logOut} />
+            </SideBar>
+            <AppPage>
+                <Outlet />
+            </AppPage>
+        </AppLayout>
+    );
 };
