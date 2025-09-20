a9b6f3569c1062b143b2c39befeb1cd063565466
Ali Ramazon <mr.safarali@gmail.com>
2025-09-20 12:00:43 -0600

    Design System: Flex: support flex item properties

--- src/design-system/Flex/Flex.tsx
+++ src/design-system/Flex/Flex.tsx
@@ -1,25 +1,83 @@
 import styled from "@emotion/styled";
 
 type FlexProps = {
-    $direction?: "row" | "column";
-    $align?: "flex-start" | "flex-end" | "center" | "stretch" | "baseline";
-    $justify?:
+    $flexDirection?: "row" | "column" | "row-reverse" | "column-reverse";
+    $alignItems?: "flex-start" | "flex-end" | "center" | "stretch" | "baseline";
+    $justifyContent?:
         | "flex-start"
         | "flex-end"
         | "center"
         | "space-between"
         | "space-around"
         | "space-evenly";
-    $wrap?: "nowrap" | "wrap" | "wrap-reverse";
+    $flexWrap?: "nowrap" | "wrap" | "wrap-reverse";
     $gap?: string;
-    $inline?: boolean;
+    $rowGap?: string;
+    $columnGap?: string;
+    $display?: "flex" | "inline-flex";
+
+    $alignContent?:
+        | "flex-start"
+        | "flex-end"
+        | "center"
+        | "stretch"
+        | "space-between"
+        | "space-around"
+        | "space-evenly";
+
+    $flex?: string;
+    $flexGrow?: number;
+    $flexShrink?: number;
+    $flexBasis?: string;
+    $alignSelf?:
+        | "auto"
+        | "flex-start"
+        | "flex-end"
+        | "center"
+        | "stretch"
+        | "baseline";
+    $order?: number;
+
+    // Sizing (works for both container and item)
+    $width?: string;
+    $height?: string;
+    $minWidth?: string;
+    $maxWidth?: string;
+    $minHeight?: string;
+    $maxHeight?: string;
 };
 
 export const Flex = styled.div<FlexProps>`
-    display: ${(props) => (props.$inline ? "inline-flex" : "flex")};
-    flex-direction: ${(props) => props.$direction || "row"};
-    align-items: ${(props) => props.$align || "stretch"};
-    justify-content: ${(props) => props.$justify || "flex-start"};
-    flex-wrap: ${(props) => props.$wrap || "nowrap"};
-    gap: ${(props) => props.$gap || "0"};
+    display: ${(props) => props.$display || "flex"};
+
+    flex-direction: ${(props) => props.$flexDirection || "row"};
+    align-items: ${(props) => props.$alignItems || "stretch"};
+    justify-content: ${(props) => props.$justifyContent || "flex-start"};
+    flex-wrap: ${(props) => props.$flexWrap || "nowrap"};
+
+    ${(props) => props.$gap && `gap: ${props.$gap};`}
+    ${(props) => props.$rowGap && `row-gap: ${props.$rowGap};`}
+    ${(props) => props.$columnGap && `column-gap: ${props.$columnGap};`}
+    
+
+    ${(props) =>
+        props.$alignContent && `align-content: ${props.$alignContent};`}
+    
+
+    ${(props) => props.$flex && `flex: ${props.$flex};`}
+    ${(props) =>
+        props.$flexGrow !== undefined && `flex-grow: ${props.$flexGrow};`}
+    ${(props) =>
+        props.$flexShrink !== undefined && `flex-shrink: ${props.$flexShrink};`}
+    ${(props) => props.$flexBasis && `flex-basis: ${props.$flexBasis};`}
+    ${(props) => props.$alignSelf && `align-self: ${props.$alignSelf};`}
+    ${(props) => props.$order !== undefined && `order: ${props.$order};`}
+    
+
+    ${(props) => props.$width && `width: ${props.$width};`}
+    ${(props) => props.$height && `height: ${props.$height};`}
+    ${(props) => props.$minWidth && `min-width: ${props.$minWidth};`}
+    ${(props) => props.$maxWidth && `max-width: ${props.$maxWidth};`}
+    ${(props) => props.$minHeight && `min-height: ${props.$minHeight};`}
+    ${(props) => props.$maxHeight && `max-height: ${props.$maxHeight};`}
 `;
